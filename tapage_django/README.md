8d8883599403f46ed0f5a105cfbed6b8dbadd2f6
gabriel.lahellec@gmail.com <gabriel.lahellec@gmail.com>
2020-03-12 10:59:45 +0100

    initial commit

---
+++ tapage_django/README.md
@@ -0,0 +1,28 @@
+# Project Title
+
+Tapage l'Seut
+
+## arbre des dossiers
+
+Voir tree.html
+
+### Prerequisites
+
+Pour faire tourner le site, executer :
+
+```
+cd tapage_django
+source tapage_django/tapage_virtual_env/bin/activate
+python manage.py runserver
+```
+
+
+## Authors
+
+* **Gabriel Lahellec** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- tapage_django/tapage_django/settings.py
+++ tapage_django/tapage_django/settings.py
@@ -188,12 +188,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/site_tapage/static/site_tapage/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'# old = '/site_tapage/static/site_tapage/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #url des fichiers statiques pour chaque application
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "forum"),
-#     )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    )
 
 #login spirit_cache
 # LOGGING = {
