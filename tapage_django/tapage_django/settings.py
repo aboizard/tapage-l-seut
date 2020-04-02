02c85cb5ddea933151ac64d2d31fe4ac7dc54ab3
Antoine Boizard <boizard@apc.in2p3.fr>
2020-04-02 10:59:32 +0200

    Edition 2017 update

--- tapage_django/tapage_django/settings.py
+++ tapage_django/tapage_django/settings.py
@@ -189,11 +189,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'# old = '/site_tapage/static/site_tapage/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #url des fichiers statiques pour chaque application
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    )
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#    )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 #login spirit_cache
 # LOGGING = {
